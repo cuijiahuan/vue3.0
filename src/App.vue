539bf7ce87f66121e3c09b659018c0ac420b40b6
cuijiahuan <jihuanc_3@163.com>
2020-01-20 10:31:21 +0800

    引入anti desigin UI

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,21 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div class="wrap">
+    <a-button type="primary">Primary</a-button>
+    <a-button>Default</a-button>
+    <a-button type="dashed">Dashed</a-button>
+    <a-button type="danger">Danger</a-button>
+    <a-config-provider :autoInsertSpaceInButton="false">
+      <a-button type="primary"></a-button>
+    </a-config-provider>
+    <a-button type="primary">按钮</a-button>
+    <a-button type="link">Link</a-button>
   </div>
 </template>
-
-<style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<style scoped>
+  .wrap{
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
-}
 </style>
